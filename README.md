198dc006f172b0a99d96f15d2923798d2a95a42a
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-11-24 17:44:37 -0600

    Customer View working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Bamazon
+
+## Client side
+
+https://drive.google.com/file/d/1P4sKQs2jnuit9MwOvCfClEiS8mXIJaxv/view
+<iframe src="https://drive.google.com/file/d/1P4sKQs2jnuit9MwOvCfClEiS8mXIJaxv/preview" width="640" height="480"></iframe>
+
--- bamazonCostumer.js
+++ bamazonCostumer.js
@@ -1,25 +1,80 @@
 require("dotenv").config();
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({ 
-    host : process.env.DB_HOST,
-    port: process.env.DB_PORT, 
-    user : process.env.DB_USER, 
-    password : process.env.DB_PASS, 
-    database : process.env.DB_NAME });
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
 
 connection.connect(function (err) {
     if (err) throw err;
     searchProducts();
-    connection.end();
 });
+
+var productList = [];
 
 function searchProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log("ID | Product | Department | Price | Stock");
         for (var i in res) {
-            console.log(res[i].item_id+" | "+res[i].product_name+" | "+res[i].department_name+" | "+res[i].price+" | "+res[i].stock_quantity);
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+            productList.push(res[i].product_name);
         };
-});
-};
+        selectProduct();
+    });
+}
+
+function selectProduct() {
+    inquirer
+        .prompt([
+            {
+                type: "rawlist",
+                message: "Which product would you like to buy?",
+                name: "productToBuy",
+                choices: productList
+            },
+            {
+                type: "number",
+                message: "How many would you like to buy?",
+                name: "quantityToBuy"
+            }
+
+        ])
+        .then(function (response) {
+            var productToBuy = response.productToBuy;
+            var quantityToBuy = response.quantityToBuy;
+            var index = productList.indexOf(productToBuy)
+            var databaseIndex = index + 1;
+            connection.query('SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?', { item_id: databaseIndex }, (err, res) => {
+                if (err) throw err;
+                var stock = res[0].stock_quantity;
+                var price = res[0].price;
+
+                if (quantityToBuy > stock) {
+                    console.log("Insufficient quantity! Only " + stock + " available\n----------\n");
+                    connection.end();
+                }
+                else {
+                    var newStock = stock - quantityToBuy;
+                    connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [newStock, databaseIndex], (err, res) => {
+                        if (err) throw err;
+                        console.log("Order confirmed! Your total is: " + price * quantityToBuy + " dollars \n----------\n");
+                        connection.end();
+                    }
+
+                    )
+                    
+                }
+
+            }
+            )
+        })
+        
+}
+
+
